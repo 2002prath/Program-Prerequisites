ee642961e16a08a42690ad45d55c206fc9495d6e
2002prath <prathammishra1111@gmail.com>
2025-01-08 01:12:58 +0530

    perimeter of rectangle

---
+++ PrRec.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+                                 //PrRec --> perimeter of rectangle
+class PrRec
+{  
+  public static void main(String[] amg)
+   { 
+     Scanner sc = new Scanner(System.in);     
+      int l = sc.nextInt(); 
+      int b = sc.nextInt(); 
+      int peri = 2*(l+b 
+        System.out.print(peri); 
+ 
+        sc.close(); 
+   } 
+}
