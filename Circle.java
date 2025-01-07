8e08e29e65cd2b81cac2bac2e2c0cf74f601ea5f
2002prath <prathammishra1111@gmail.com>
2025-01-08 00:47:14 +0530

    area of circle Added

---
+++ Circle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+
+class Circle
+{ 
+  public static void main(String[] amg)
+   { 
+     Scanner sc = new Scanner(System.in); 
+       double r = sc.nextInt(); 
+       float pi = 3.14; 
+       double area = pi* r * r; 
+       System.out.println(area); 
+   } 
+}
+      
+ 
