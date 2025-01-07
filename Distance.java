2cfe3a04a3ad8f459a63d585e9d2595344483ed7
2002prath <prathammishra1111@gmail.com>
2025-01-08 01:24:33 +0530

    conversion from km into miles

---
+++ Distance.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+                                 //Conversion of distance value from kilometer to mile 
+class Distance
+{  
+  public static void main(String[] amg)
+   { 
+     Scanner sc = new Scanner(System.in);     
+      int Kilometers = sc.nextInt(); 
+      
+       int Miles = Kilometers * 0.621371.
+        System.out.print(Miles); 
+ 
+        sc.close(); 
+   } 
+}
